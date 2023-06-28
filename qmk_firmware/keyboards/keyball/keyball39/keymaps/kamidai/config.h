192bc0e854431078150c4a25277c3576a0c238ea
RinGoku <skyta80105@gmail.com>
2023-06-28 15:56:56 +0900

    change color by layer

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/kamidai/config.h
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/kamidai/config.h
@@ -21,10 +21,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #pragma once
 
 // 容量削減のため以下をオミット
-// #ifdef RGBLIGHT_ENABLE
+#ifdef RGBLIGHT_ENABLE
 // #define RGBLIGHT_EFFECT_BREATHING
 // #define RGBLIGHT_EFFECT_RAINBOW_MOOD
-// #define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+#define RGBLIGHT_EFFECT_RAINBOW_SWIRL
 // #define RGBLIGHT_EFFECT_SNAKE
 // #define RGBLIGHT_EFFECT_KNIGHT
 // #define RGBLIGHT_EFFECT_CHRISTMAS
@@ -32,7 +32,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define RGBLIGHT_EFFECT_RGB_TEST
 // #define RGBLIGHT_EFFECT_ALTERNATING
 // #define RGBLIGHT_EFFECT_TWINKLE
-// #endif
+#endif
 
 // #define COMBO_COUNT 2
 
--- qmk_firmware/keyboards/keyball/keyball39/keymaps/kamidai/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/kamidai/keymap.c
@@ -114,17 +114,33 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 layer_state_t layer_state_set_user(layer_state_t state) {
   // レイヤーが1または3の場合、スクロールモードが有効になる
-  // keyball_set_scroll_mode(get_highest_layer(state) == 1 || get_highest_layer(state) == 3);
-  keyball_set_scroll_mode(get_highest_layer(state) == 1);
+  keyball_set_scroll_mode(get_highest_layer(state) == 1 || get_highest_layer(state) == 3);
+  // keyball_set_scroll_mode(get_highest_layer(state) == 3);
 
+  // rgblight_mode_noeeprom(RGBLIGHT_MODE_RAINBOW_SWIRL);
   // レイヤーとLEDを連動させる
   switch (get_highest_layer(state)) {
+    case 1:
+      rgblight_sethsv(HSV_MAGENTA);
+      break;
+    case 2:
+      rgblight_sethsv(HSV_BLUE);
+      break;
+    case 3:
+      rgblight_sethsv(HSV_YELLOW);
+      break;
+    case 4:
+      rgblight_sethsv(HSV_RED);
+      break;
+    case 5:
+      rgblight_sethsv(HSV_GREEN);
+      break;
     case click_layer:
-      rgblight_sethsv(HSV_WHITE);
+      rgblight_sethsv(HSV_TURQUOISE);
       break;
 
     default:
-      rgblight_sethsv(HSV_OFF);
+      rgblight_sethsv(HSV_PURPLE);
   }
 
   return state;
